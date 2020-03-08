410bedf177f6566ac9e395b688c8ac394642c794
Jesus Seijas <jesus.seijas@axa.com>
2020-03-08 23:56:06 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,123 @@
+# @caijs/eval
+
+@caijs/eval is like eval, but executed with a context.
+static-eval is like eval. It is intended for use in build scripts and code transformations, doing some evaluation at build time—it is NOT suitable for handling arbitrary untrusted user input. Malicious user input can execute arbitrary code.
+
+## Installation
+
+In your project folder run:
+
+```bash
+$ npm install @caijs/eval
+```
+
+## Examples of use
+
+You can evaluate expressions
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = '[1, 2, 3].map(function(x) { return x * 2 })';
+const result = evaluate(src);
+console.log(result); // [2, 4, 6]
+```
+
+You can add a context
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = '[1, 2, 3].map(function(x) { return x * n })';
+const context = { n: 3 };
+const result = evaluate(src, context);
+console.log(result); // [3, 6, 9]
+```
+
+You can also declare functions in the context:
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = '[1,2,3+4*10+n,foo(3+5),obj[""+"x"].y]';
+const context = {
+  n: 6,
+  foo(x) {
+    return x * 100;
+  },
+  obj: { x: { y: 555 } },
+};
+const result = evaluate(src, context);
+console.log(result); // [ 1, 2, 49, 800, 555 ]
+```
+
+You can use a "this" if you define it in the context:
+
+```javascript
+const { evaluate } = require('./src/evaluator');
+
+const src = 'this.a + this.b + n';
+const context = {
+  this: {
+    a: 1,
+    b: 2
+  },
+  n: 6,
+};
+const result = evaluate(src, context);
+console.log(result);
+```
+
+You can modify context variables:
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = 'this.a += this.b; this.a';
+const context = {
+  this: {
+    a: 1,
+    b: 2,
+  },
+  n: 6,
+};
+const result = evaluate(src, context);
+console.log(result); // 3
+console.log(context); // { this: { a: 3, b: 2 }, n: 6 }
+```
+
+If you don't want the code to modify your context, pass a "false" as the last parameter:
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = 'this.a += this.b; this.a';
+const context = {
+  this: {
+    a: 1,
+    b: 2,
+  },
+  n: 6,
+};
+const result = evaluate(src, context, false);
+console.log(result); // 1
+console.log(context); // { this: { a: 1, b: 2 }, n: 6 }
+```
+
+You can even create your own context variables:
+
+```javascript
+const { evaluate } = require('@caijs/eval');
+
+const src = 'z = this.a + this.b; z';
+const context = {
+  this: {
+    a: 1,
+    b: 2,
+  },
+  n: 6,
+};
+const result = evaluate(src, context);
+console.log(result); // 3
+console.log(context); // { this: { a: 1, b: 2 }, n: 6, z: 3 }
+```
---
+++ src/evaluator.js
@@ -0,0 +1,431 @@
+const { generate: unparse } = require('escodegen');
+const { parse } = require('esprima');
+
+/* eslint no-use-before-define: 0 */
+
+const failResult = {};
+function walkLiteral(node) {
+  return node.value;
+}
+
+function walkUnary(node, context) {
+  switch (node.operator) {
+    case '+':
+      return +walk(node.argument, context);
+    case '-':
+      return -walk(node.argument, context);
+    case '~':
+      /* eslint-disable no-bitwise */
+      return ~walk(node.argument, context);
+    case '!':
+      return !walk(node.argument, context);
+    default:
+      return failResult;
+  }
+}
+
+function walkArray(node, context) {
+  const result = [];
+  for (let i = 0, l = node.elements.length; i < l; i += 1) {
+    const x = walk(node.elements[i], context);
+    if (x === failResult) {
+      return failResult;
+    }
+    result.push(x);
+  }
+  return result;
+}
+
+function walkObject(node, context) {
+  const result = {};
+  for (let i = 0, l = node.properties.length; i < l; i += 1) {
+    const prop = node.properties[i];
+    const value = walk(prop.value, context);
+    if (value === failResult) {
+      return failResult;
+    }
+    result[prop.key.value || prop.key.name] = value;
+  }
+  return result;
+}
+
+function walkBinary(node, context) {
+  const left = walk(node.left, context);
+  if (left === failResult) {
+    return failResult;
+  }
+  const right = walk(node.right, context);
+  if (right === failResult) {
+    return failResult;
+  }
+  switch (node.operator) {
+    case '==':
+      /* eslint-disable eqeqeq */
+      return left == right;
+    case '===':
+      return left === right;
+    case '!=':
+      /* eslint-disable eqeqeq */
+      return left != right;
+    case '!==':
+      return left !== right;
+    case '+':
+      return left + right;
+    case '-':
+      return left - right;
+    case '*':
+      return left * right;
+    case '/':
+      return left / right;
+    case '%':
+      return left % right;
+    case '<':
+      return left < right;
+    case '<=':
+      return left <= right;
+    case '>':
+      return left > right;
+    case '>=':
+      return left >= right;
+    case '|':
+      /* eslint-disable no-bitwise */
+      return left | right;
+    case '&':
+      /* eslint-disable no-bitwise */
+      return left & right;
+    case '^':
+      /* eslint-disable no-bitwise */
+      return left ^ right;
+    case '||':
+      return left || right;
+    case '&&':
+      return left && right;
+    default:
+      return failResult;
+  }
+}
+
+function walkIdentifier(node, context) {
+  if ({}.hasOwnProperty.call(context, node.name)) {
+    return context[node.name];
+  }
+  return failResult;
+}
+
+function walkThis(node, context) {
+  if ({}.hasOwnProperty.call(context, 'this')) {
+    // eslint-disable-next-line
+    return context["this"];
+  }
+  return failResult;
+}
+
+function walkCall(node, context) {
+  const callee = walk(node.callee, context);
+  if (callee === failResult || typeof callee !== 'function') {
+    return failResult;
+  }
+  let ctx = node.callee.object ? walk(node.callee.object, context) : failResult;
+  if (ctx === failResult) {
+    ctx = null;
+  }
+  const args = [];
+  for (let i = 0, l = node.arguments.length; i < l; i += 1) {
+    const x = walk(node.arguments[i], context);
+    if (x === failResult) {
+      return failResult;
+    }
+    args.push(x);
+  }
+  return callee.apply(ctx, args);
+}
+
+function walkMember(node, context) {
+  const obj = walk(node.object, context);
+  if (obj === failResult || typeof obj === 'function') {
+    return failResult;
+  }
+  if (node.property.type === 'Identifier') {
+    return obj[node.property.name];
+  }
+  const prop = walk(node.property, context);
+  if (prop === failResult) {
+    return failResult;
+  }
+  return obj[prop];
+}
+
+function walkConditional(node, context) {
+  const value = walk(node.test, context);
+  if (value === failResult) {
+    return failResult;
+  }
+  if (value) {
+    return walk(node.consequent, context);
+  }
+  if (!node.alternate) {
+    return undefined;
+  }
+  return walk(node.alternate, context);
+}
+
+function walkExpression(node, context) {
+  const value = walk(node.expression, context);
+  if (value === failResult) {
+    return failResult;
+  }
+  return value;
+}
+
+function walkReturn(node, context) {
+  return walk(node.argument, context);
+}
+
+function walkFunction(node, context) {
+  const newContext = {};
+  const keys = Object.keys(context);
+  keys.forEach(element => {
+    newContext[element] = context[element];
+  });
+  node.params.forEach(key => {
+    if (key.type === 'Identifier') {
+      newContext[key.name] = null;
+    }
+  });
+  const bodies = node.body.body;
+  for (let i = 0, l = bodies.length; i < l; i += 1) {
+    if (walk(bodies[i], newContext) === failResult) {
+      return failResult;
+    }
+  }
+  const vals = keys.map(key => context[key]);
+  // eslint-disable-next-line
+  return Function(keys.join(', '), 'return ' + unparse(node)).apply(
+    null,
+    vals
+  );
+}
+
+function walkTemplateLiteral(node, context) {
+  let str = '';
+  for (let i = 0; i < node.expressions.length; i += 1) {
+    str += walk(node.quasis[i], context);
+    str += walk(node.expressions[i], context);
+  }
+  return str;
+}
+
+function walkTemplateElement(node) {
+  return node.value.cooked;
+}
+
+function walkTaggedTemplate(node, context) {
+  const tag = walk(node.tag, context);
+  const { quasi } = node;
+  const strings = quasi.quasis.map(q => walk(q, context));
+  const values = quasi.expressions.map(e => walk(e, context));
+  // eslint-disable-next-line
+  return tag.apply(null, [strings].concat(values));
+}
+
+function walkUpdateExpression(node, context) {
+  let value = walk(node.argument, context);
+  if (value === failResult) {
+    return failResult;
+  }
+  switch (node.operator) {
+    case '++':
+      value += 1;
+      return walkSet(node.argument, context, value);
+    case '--':
+      value -= 1;
+      return walkSet(node.argument, context, value);
+    default:
+      return failResult;
+  }
+}
+
+function walkAssignmentExpression(node, context) {
+  const value = walk(node.right, context);
+  if (value === failResult) {
+    return failResult;
+  }
+  let leftValue = walk(node.left, context);
+  if (leftValue === failResult) {
+    leftValue = 0;
+  }
+  switch (node.operator) {
+    case '=':
+      walkSet(node.left, context, value);
+      return value;
+    case '+=':
+      leftValue += value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '-=':
+      leftValue -= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '*=':
+      leftValue *= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '/=':
+      leftValue /= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '%=':
+      leftValue %= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '|=':
+      // eslint-disable-next-line
+      leftValue |= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '&=':
+      // eslint-disable-next-line
+      leftValue &= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    case '^=':
+      // eslint-disable-next-line
+      leftValue ^= value;
+      walkSet(node.left, context, leftValue);
+      return leftValue;
+    default:
+      return failResult;
+  }
+}
+
+function walkBlock(node, context) {
+  if (Array.isArray(node.body)) {
+    let result;
+    for (let i = 0; i < node.body.length; i += 1) {
+      result = walk(node.body[i], context);
+    }
+    return result;
+  }
+  return walk(node.body, context);
+}
+
+function walk(node, context) {
+  switch (node.type) {
+    case 'Literal':
+      return walkLiteral(node, context);
+    case 'UnaryExpression':
+      return walkUnary(node, context);
+    case 'ArrayExpression':
+      return walkArray(node, context);
+    case 'ObjectExpression':
+      return walkObject(node, context);
+    case 'BinaryExpression':
+    case 'LogicalExpression':
+      return walkBinary(node, context);
+    case 'Identifier':
+      return walkIdentifier(node, context);
+    case 'ThisExpression':
+      return walkThis(node, context);
+    case 'CallExpression':
+      return walkCall(node, context);
+    case 'MemberExpression':
+      return walkMember(node, context);
+    case 'ConditionalExpression':
+      return walkConditional(node, context);
+    case 'ExpressionStatement':
+      return walkExpression(node, context);
+    case 'ReturnStatement':
+      return walkReturn(node, context);
+    case 'FunctionExpression':
+      return walkFunction(node, context);
+    case 'TemplateLiteral':
+      return walkTemplateLiteral(node, context);
+    case 'TemplateElement':
+      return walkTemplateElement(node, context);
+    case 'TaggedTemplateExpression':
+      return walkTaggedTemplate(node, context);
+    case 'UpdateExpression':
+      return walkUpdateExpression(node, context);
+    case 'AssignmentExpression':
+      return walkAssignmentExpression(node, context);
+    case 'IfStatement':
+      return walkConditional(node, context);
+    case 'BlockStatement':
+      return walkBlock(node, context);
+    default:
+      return failResult;
+  }
+}
+
+function walkSetIdentifier(node, context, value) {
+  const newContext = context;
+  newContext[node.name] = value;
+  return value;
+}
+
+function walkSetMember(node, context, value) {
+  const obj = walk(node.object, context);
+  if (obj === failResult || typeof obj === 'function') {
+    return failResult;
+  }
+  if (node.property.type === 'Identifier') {
+    obj[node.property.name] = value;
+    return value;
+  }
+  const prop = walk(node.property, context);
+  if (prop === failResult) {
+    return failResult;
+  }
+  obj[prop] = value;
+  return value;
+}
+
+function walkSet(node, context, value) {
+  switch (node.type) {
+    case 'Identifier':
+      return walkSetIdentifier(node, context, value);
+    case 'MemberExpression':
+      return walkSetMember(node, context, value);
+    default:
+      return failResult;
+  }
+}
+
+function deepFreeze(o) {
+  Object.freeze(o);
+  Object.getOwnPropertyNames(o).forEach(function(prop) {
+    if (
+      Object.prototype.hasOwnProperty.call(o, prop) &&
+      o[prop] !== null &&
+      (typeof o[prop] === 'object' || typeof o[prop] === 'function') &&
+      !Object.isFrozen(o[prop])
+    ) {
+      deepFreeze(o[prop]);
+    }
+  });
+  return o;
+}
+
+function evaluateAll(str, context = {}, allowChanges = true) {
+  const newContext = allowChanges ? context : deepFreeze(context);
+  const result = [];
+  const compiled = parse(str);
+  for (let i = 0; i < compiled.body.length; i += 1) {
+    const expression = compiled.body[i].expression
+      ? compiled.body[i].expression
+      : compiled.body[i];
+    const value = walk(expression, newContext);
+    result.push(value === failResult ? undefined : value);
+  }
+  return result;
+}
+
+function evaluate(str, context = {}, allowChanges) {
+  const result = evaluateAll(str, context, allowChanges);
+  if (!result || result.length === 0) {
+    return undefined;
+  }
+  return result[result.length - 1];
+}
+
+module.exports = { evaluateAll, evaluate, deepFreeze };
